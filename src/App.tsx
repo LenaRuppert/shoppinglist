359957212081712434e5505b63f511c7834941b5
lenaruppert <l.zi@gmx.de>
2023-02-18 15:19:02 +0100

    create AddItem

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import './App.css';
 import Header from "./component/Header";
 import {Item} from "./model/Item";
 import ItemList from "./component/ItemList";
+import AddItem from "./component/AddItem";
 
 
 function App() {
 
     const [items, setItems] = useState<Item[]>([{name: "Paprika", id: "1"}])
 
+    function addItem(newItem: Item){
+        setItems(prevState => [...prevState, newItem])
+    }
+
   return <div className="App">
    <Header/>
       <ItemList items={items}/>
+      <AddItem onAddItem={addItem}/>
 
   </div>
 
---
+++ src/component/AddItem.tsx
@@ -0,0 +1,28 @@
+import React, {ChangeEvent, useState} from "react";
+import {Item} from "../model/Item";
+import "./AddItem.css"
+
+type AddItemProps = {
+    onAddItem: (newItem: Item) => void
+}
+
+export default function AddItem(props: AddItemProps){
+
+    const[name, setName] = useState<string>("")
+
+    function onNameChange(event: ChangeEvent<HTMLInputElement>){
+        setName(event.target.value)
+    }
+
+    function onNameSave(){
+        props.onAddItem({name, id:"1"})
+    }
+
+    return(
+        <div className={"additem"}>
+            <label>neues Produkt</label>
+            <input value={name} onChange={onNameChange}/>
+            <button onClick={onNameSave}>ok</button>
+        </div>
+    )
+}
